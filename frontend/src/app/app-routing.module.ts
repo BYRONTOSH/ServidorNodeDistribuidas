3d609ab77b7b7e29087574cc52b325fd157c7a20
BYRON LOARTE <by_tosh20@hotmail.com>
2021-02-28 09:14:55 -0500

    Add files via upload

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CategoriaComponent } from './components/categoria/categoria.component';
+import { ProductoComponent } from './components/producto/producto.component';
+
+const routes: Routes = [
+  
+  { path: 'categoria', component: CategoriaComponent },
+  { path: 'producto', component: ProductoComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', pathMatch:'full', redirectTo:'home'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
